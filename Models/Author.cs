06aa2b775b8a94404c1c3b4d3c200412fbf13719
Arthur Fücher <arthurfucher@gmail.com>
2017-04-20 22:58:43 -0300

    first commit

---
+++ Models/Author.cs
@@ -0,0 +1,3 @@
+public class Author{
+    public string Name {get; set;}
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using Dapper;
+using Npgsql;
+
+namespace hwapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var sqlConnection =
+        new NpgsqlConnection("Host=localhost;Username=postgres;Password=postgres;Database=dotnet"))
+            {
+                sqlConnection.Open();
+
+                IEnumerable authors =
+                    sqlConnection.Query("Select * from Authors");
+
+                foreach (Author author in authors)
+                {
+                    Console.Write(author);
+                }
+
+                sqlConnection.Close();
+            }
+        }
+    }
+}
